e3032b8beae4475b0093cec8db49ffe26c25c030
Abigail Young <Abigail@sherry.local>
2014-04-06 18:04:32 -0700

    Cleaning up unused files

--- forms.py
+++
@@ -1,8 +0,0 @@
-from flask.ext.wtf import Form
-from flask_wtf.html5 import URLField
-from wtforms import TextField
-from wtforms.validators import Required, url
-
-# class LinkForm(Form):
-#     def __init__(self, require_tld=False, message="Error!"):
-#         self.page_url = URLField(validators=[url()])
