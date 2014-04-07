63a6a4a33218252887cde8db0e39dcd3e3656caf
Abigail Young <Abigail@sherry.local>
2014-04-07 15:09:41 -0700

    Cleaning up

--- model.py
+++ model.py
@@ -30,7 +30,7 @@ Base.query = db_session.query_property()
 
 
 
-
+# Create table of error messages
 class Message(Base):
     __tablename__ = "messages" 
     
@@ -39,7 +39,7 @@ class Message(Base):
     message = Column(Text, nullable=False)
     error = Column(Text, nullable=False)
 
-
+# Create table of error messages per report
 class Report_Message(Base):
     
     __tablename__ = "report_message"
@@ -50,8 +50,7 @@ class Report_Message(Base):
     report = relationship("Report", backref=backref("messages", order_by=report_id))
     message = relationship("Message")
 
-
-
+# Create table of reports
 class Report(Base):
     __tablename__ = "reports"
     
@@ -71,9 +70,10 @@ def create_tables():
 
 class PageParser():
     def __init__(self, url=None, html=None):
+        # Accept either html or url for parsing
         if html:
-            print "THIS IS HTML"
-            print html
+            # print "THIS IS HTML"
+            # print html
             self.soup = BeautifulSoup(html)
         
         elif url:
@@ -81,8 +81,8 @@ class PageParser():
             self.r = requests.get(self.url)
 
             self.data = self.r.text
-            print "THIS IS FROM URL"
-            print self.data
+            # print "THIS IS FROM URL"
+            # print self.data
             self.soup = BeautifulSoup(self.data)
         
         else:
@@ -98,14 +98,18 @@ class PageParser():
         self.num_replaced = False
 
 
-    # Generate number of headings and links on page
+    
     def headings(self):
+        # Generate number of headings and links on page
         headings = []
         for tag in self.soup.findAll(re.compile('^h\d')):
             headings.append(tag.name)
+        
         return headings
 
+
     def stats(self):
+        # Count number of links and headings
         stats = {}
 
         num_links = len(self.links())
@@ -116,17 +120,21 @@ class PageParser():
         
         return stats
 
-    # Generate page outline of all headings
+    
     def outline(self):
-        outline = {}
+        # Generate page outline of all headings
+        outline = []
         for heading in self.soup.findAll(re.compile('^h\d')):
             #insert h at end of string
-            outline[heading.text] = heading.name
+            outline_dict = {}
+            outline_dict[heading.text] = heading.name
+            outline.append(outline_dict)
         
         return outline
 
-    # Add in table stats to body
+    
     def tables(self):
+        # Add in table stats to body
         tables = self.soup.findAll('table')
 
         for i in range(len(tables)):
@@ -143,34 +151,34 @@ class PageParser():
             my_table.insert(0, "Table with %s rows and %s columns. " % (row_count,column_count))
 
     def extract(self):
-        #clean html comments
+        # Clean html comments
         comments = self.soup.findAll(text=lambda text:isinstance(text, Comment))
         [comment.extract() for comment in comments]
 
         doctype = self.soup.findAll(text=lambda text:isinstance(text, bs4.Doctype))
         [doc.extract() for doc in doctype]
         
-        #clean script tags
+        # Clean script tags
         scripts = self.soup.findAll('script')
         [script.extract() for script in scripts]
         
-        #clean meta tags
+        # Clean meta tags
         meta = self.soup.findAll('meta')
         [met.extract() for met in meta]
 
-        #clean style
+        # Clean style
         styles = self.soup.findAll('style')
         [style.extract() for style in styles]
 
     
     def landmarks(self):
        
-        #print LINK for <a>
+        # Print LINK for <a>
         for a in self.soup.findAll('a'):
             a.attrs['class'] = "link"
             a.insert(0, ' {{LINK}} ')
         
-        #print GRAPHIC and alt text for img
+        # Print GRAPHIC and alt text for img
         imgs = self.soup.findAll('img')
         for img in imgs:
             if img.has_attr('alt'):
@@ -179,15 +187,15 @@ class PageParser():
             else:
                 break
         
-        #print BULLET in front of <li>
+        # Print BULLET in front of <li>
         for li in self.soup.findAll('li'):
             li.insert(0, ' {{BULLET}} ')
         
-        #print FORM in front of <form>
+        # Print FORM in front of <form>
         for form in self.soup.findAll('form'):
             form.insert(0, ' {{FORM}} ')
 
-        #print heading level
+        # Print heading level
         for heading in self.soup.findAll(re.compile('^h\d')):
             
             if heading.name == "h1":
@@ -207,7 +215,7 @@ class PageParser():
     def char_replacement(self):
         
         body = self.soup.get_text()
-        #converts special characters. needs to be before num2words
+        # Converts special characters. Needs to be before num2words.
         characters = {
                         '-': " dash ",
                         '>': " greater ",
@@ -277,8 +285,9 @@ class PageParser():
         self.num_replaced = True
         return body    
 
-    # Create list of all links on page    
+       
     def links(self):
+        # Create list of all links on page 
         links = []
         
         for tag in self.soup.findAll(re.compile('^a')):
@@ -286,8 +295,9 @@ class PageParser():
 
         return links
 
-    # Generate printed list of all links
+    
     def links_list(self):
+        # Generate printed list of all links
         links = self.links()
         links_list = []
         for i in range(len(links)):
@@ -301,8 +311,11 @@ class PageParser():
         for tag in self.soup.findAll(re.compile('title')):
             title.append(tag)
         return title[0]
+
     # Warnings/Errors Checks
+
     def check_title(self):
+        # Does page have properly used <title>?
         title = []
         for tag in self.soup.findAll(re.compile('title')):
             title.append(tag)
@@ -315,14 +328,14 @@ class PageParser():
             return "titlenone"
 
     def img_alt(self):
-        #check if alt tag present. if not, throw error
+        # Check if alt tag present
         imgs = self.soup.findAll('img')
         for img in imgs:
             if img.has_attr('alt'):
                 return "True"
             else:
                 return "alttags"
-                #TODO: check for where in code missing alt tags are
+                
 
 
     def headings_check(self):
@@ -362,6 +375,7 @@ class PageParser():
         
 
     def redundant_link(self):
+        # Check for/gather redundant links
         hrefs = []
         
         redundant_links = []
@@ -377,6 +391,7 @@ class PageParser():
         return redundant_links
 
     def empty_links(self):
+        # Check for/gather empty links
         links_inner = {}
         empty_links = {'URLS': [], 'Total': 0}
 
@@ -389,6 +404,7 @@ class PageParser():
         return empty_links
 
     def layout_table(self):
+        # Are layout tables used?
         for tag in self.soup.findAll('table'):
             if self.soup.th:
                 return "True"
@@ -396,14 +412,14 @@ class PageParser():
                 return "layouttables"
 
     def language(self):
-        
+        # Is a language declared?
         if self.soup.html.has_attr('lang'):
             return "True"
         else:
             return "language"
 
     def input(self):
-        #check if <input> has labels
+        # Check if <input> has label
         if self.soup.findAll('input'):
             for sibling in self.soup.input.previous_siblings:
                 sib = (repr(sibling))
@@ -412,7 +428,8 @@ class PageParser():
                 else:
                     return "inputlabel"
 
-    def text_area(self):    
+    def text_area(self):  
+        # Check if <textarea> has label  
         if self.soup.findAll('textarea'):
             for sibling in self.soup.input.previous_siblings:
                 sib = (repr(sibling))
@@ -421,7 +438,8 @@ class PageParser():
                 else:
                     return "textarealabel"
 
-    def select(self):    
+    def select(self):
+        # Check if <select> has label    
         if self.soup.findAll('select'):
             for sibling in self.soup.input.previous_siblings:
                 sib = (repr(sibling))
@@ -431,17 +449,20 @@ class PageParser():
                     return "selectlabel"
 
     def noscript(self):
+        # Check for <noscript> tag
         if self.soup.findAll('noscript'):
             return 'noscript'
         else:
             return None
 
     def header(self):
+        # Check for <header> tag
         if self.soup.findAll('header'):
             return 'header'
 
         
     def checks(self):
+        # Add check results to checks dict
         checks = {}
         checks['Title'] = self.check_title()
         checks['Redundant Links'] = self.redundant_link()
@@ -458,7 +479,7 @@ class PageParser():
         return checks
 
     def parse(self):
-        
+        # Parse page
         self.extract()
         self.char_replacement()
         self.replace_numbers()
@@ -467,6 +488,7 @@ class PageParser():
         self.is_parsed = True
     
     def get_links(self):
+        # Get links list, as long as characers and numbers are replaced
         if not self.char_replaced:
             self.char_replacement()
 
@@ -479,18 +501,21 @@ class PageParser():
         return self.pagelinks
 
     def get_stats(self):
+        # Get page stats
         if self.pagestats == []:
             self.pagestats = self.stats()
 
         return self.pagestats
 
     def get_outline(self):
+        # Get page outline
         if self.pageoutline == []:
             self.pageoutline = self.outline()
 
         return self.pageoutline
 
     def get_body(self):
+        # Get body only if page is parsed
         if not self.is_parsed:
             self.parse()
 
@@ -499,11 +524,10 @@ class PageParser():
 
         return self.pagebody
 
-
-
 ## End class delcarations
+
 def add_message(report, key, value, error, code_snippet):
-    
+    # Add error messages for report to db
     message = db_session.query(Message).filter_by(error=error).first()
     message_id = message.id
     m = Report_Message(message_id=message_id, code_snippet=code_snippet)
@@ -527,6 +551,7 @@ def results(page, url):
     links = stats['Number of Links']
     links = num2words(links)
 
+    # Format body for web. Replace new lines with spaces, add markup for styling
     body = Markup(page.get_body())
     body = body.replace('\n', (' '))
     body = body.replace('{{', Markup('<span class="highlight">'))
@@ -538,11 +563,11 @@ def results(page, url):
     store_outline = json.dumps(outline)
     store_links = json.dumps(links_list)
     store_stats = json.dumps(stats)
-    # Create report
-   
+    
+    # Create and store report
     r = Report(url=url, text_output=body, links=store_links, outline=store_outline, stats=store_stats)
+    
     # Run checks
-
     checks = page.checks()
     page_report = {}
 
@@ -610,7 +635,7 @@ def results(page, url):
         page_report['Missing Select Label'] = message
 
     report_id = r.id
-    
+    # Add report details to dict
     results['headings'] = headings
     results['links'] = links
     results['body'] = body
--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request, g, session, url_for, flash, Markup, make_response
+from flask import Flask, render_template, redirect, request, g, url_for, flash, Markup, make_response
 from model import Report, Message, Report_Message, PageParser
 from flaskext.markdown import Markdown
 import config
@@ -28,6 +28,7 @@ def index():
 @app.route("/", methods=["POST"])
 def get_url():
     url = request.form.get("url")
+    # Check url for protocol
     if not re.match('^(http|https)://', url):
         url = 'http://' + url
      
@@ -36,42 +37,49 @@ def get_url():
 @app.route("/api", methods=["POST"])
 @crossdomain(origin="*")
 def get_bookmarklet():
-   
+    # Get page data
     html = request.form.get('html')
     url = request.form.get('url')
-    
+    # Parse page
     page = model.PageParser(html=html, url=url)
+    # Run checks, store in database
     results = model.results(page, url)
-
+    # Return report_id for pulling report
     resp = make_response(json.dumps(results['report_id']))
     
     return resp
 
 @app.route("/report/<data>", methods=["GET"])
 def bookmarklet_results(data):
+    # Get report_id
     report_id = data
+    # Get report from db
     report = model.db_session.query(model.Report).filter_by(id=report_id).first()
     url = report.url
+    # Format url for display
     url = re.sub('^(http|https)://', '', url)
-    
+    # Load data for display
     stats = json.loads(report.stats)
     headings = num2words(stats['Number of Headings'])
     links = num2words(stats['Number of Links'])
-
     body = Markup(report.text_output)
     outline = json.loads(report.outline)
     links_list = json.loads(report.links)
+    # Get report errors messages from db
     page_report = model.db_session.query(model.Report_Message).filter_by(report_id=report_id).all()
     
+    # Display number of issues in Issues tab
     if len(page_report) > 0:
         issues = len(page_report)
     else:
         issues = None
         page_report = ["No issues."]
+    
+    # Assemble error messages for display
     messages = {}
 
     for i in range(len(page_report)):
-        print page_report[i]
+
         msg_title = page_report[i].message.title
         msg = page_report[i].message.message
         
@@ -81,7 +89,7 @@ def bookmarklet_results(data):
         else:
             messages[msg_title] = msg
 
-    print messages
+    
     
     html = render_template("bk_results.html", headings=headings, links=links, body=body, 
                     outline=outline, links_list=links_list, messages=messages, url=url, issues=issues)
@@ -90,8 +98,10 @@ def bookmarklet_results(data):
 
 @app.route("/results", methods=["GET"])
 def results():
-  
+    # Get url to process
     url = request.args.get("url")
+    # Ir url can be reached, parse
+    # If url can't be reached, return error
     try:
         page = model.PageParser(url=url)
     except Exception, e:
@@ -99,8 +109,11 @@ def results():
         flash('Couldn\'t get page. Please try again!')
         return redirect(url_for("index"))
     
+    # Run checks and store in db
     results = model.results(page, url)
+    # Format url for display
     url = re.sub('^(http|https)://', '', url)
+    # Display number of issues in Issues tab
     if len(results['page_report']) > 0:
         issues = len(results['page_report'])
     else:
