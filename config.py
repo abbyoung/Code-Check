5b75164ff3cae7cad4699612a6b509ee598ba9e8
Abigail Young <Abigail@sherry.local>
2014-06-08 18:55:06 -0700

    Fixing debugging stuff

--- config.py
+++ config.py
@@ -1,5 +1,6 @@
 import os
 
 # Config file, put all your keys and passwords and whatnot in here
+DEBUG = True
 DB_URI = os.environ.get("DATABASE_URL", "sqlite:///my_app.db")
 # SECRET_KEY = "this should be a secret"
